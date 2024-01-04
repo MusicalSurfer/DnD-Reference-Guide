aae8461236dd0bdab7a40e2d5c9e0057e1ec1d61
DANIEL_GRADY <anakria977@gmail.com>
2024-01-04 15:05:00 -0500

    Work on styling and refine scope to fully include the spell category

--- script.js
+++ script.js
@@ -24,47 +24,46 @@ const dndReference = {
         $('#searchForm').on('click', dndReference.categoryHandler)
     },
 
-    // Event handler for when category is selected.
-    categoryHandler: () => {
-        dndReference.category = $('input[name="category"]:checked').val(); // Update category with new selection.
-        $('#searchBar').prop('placeholder', dndReference.category); // Replace temp text in search bar with current category.
-    },
-
     // Function for assigning event listener to search button.
     searchButtonClick: () => {
         $('#searchButton').click(dndReference.searchHandler);
     },
 
     // Event handler for searchButtonClick that checks the user input and category to fetch data from api.
-    searchHandler: (e) => {
+    searchHandler: () => {
+        $('#results').empty();
         const $searchBar = $('#searchBar');
-        const category = $('input[name="category"]:checked').val(); // Store the value of category selected.
         const userInput = $searchBar.val().toLowerCase();
         const encodedInput = encodeURIComponent(userInput); // Encode user input
-        const searchUrl = dndReference.apiUrl + '/api/' + category + '?name=' + encodedInput; // Final URL to search
+        const searchUrl = dndReference.apiUrl + '/api/' + dndReference.category + '?name=' + encodedInput; // Final URL to search
 
-        $('#results').empty();
-        $('#searchBar').prop('placeholder', category);
-
+        // Perform get request for main result page.
         $.get(searchUrl, (data) => {
             for (obj of data.results) {
-                console.log(obj);
                 dndReference.createMainResultCard(obj);
             }
         })
+    },
+
+    // Event handler for when category is selected.
+    categoryHandler: () => {
+        dndReference.category = $('input[name="category"]:checked').val(); // Update category with new selection.
+        $('#searchBar').prop('placeholder', dndReference.category); // Replace temp text in search bar with current category.
     },
 
     createMainResultCard: (obj) => {
         const name = obj.name;
         const url = dndReference.apiUrl + obj.url;
         const $results = $('#results');
-        const $resultCard = $(`<span class="result-card"></span>`)
+        const $resultCard = $(`<div class="card"></div>`)
         const $cardTitle = $(`<h3 class="card-title">${name}</h3>`);
         const $moreInfoButton = $(`<button type="button" class="btn btn-secondary">More Info</button>`);
-        // if ()
-        $moreInfoButton.click(() => {
-            dndReference.spellsButtonHandler(url)
-        });
+
+        if (dndReference.category === 'spells') {
+            $moreInfoButton.click(() => {
+                dndReference.spellsButtonHandler(url)
+            });
+        }
 
         $results.append($resultCard).append($cardTitle).append($moreInfoButton);
     },
@@ -73,59 +72,45 @@ const dndReference = {
         $('#results').empty();
         $.get(url, (data) => {
             const name = data.name;
-            const $resultCard = $(`<span class="result-card"></span>`)
             const $moreTitle = $(`<h3 class="more-title">${name}</h3>`);
+            let valTopicArr = []; // Empty array for data to be filled by following edge case.
+            let titleTopicArr = [];
+            let descArr = data.desc;
+            $('#results').append($moreTitle);
 
-            for (dataObj in data) {
-
-                const $moreTopic = $(`<h5 class="more-topic">${dataObj}</h5>`);
-
-                let $moreDescription = $(`<p class="more-description">${data[dataObj]}</p>`)
-
-                $('#results').append($resultCard);
-                $resultCard.append($moreTitle);
-                $moreTitle.append($moreTopic);
-                $moreTopic.append($moreDescription);
-                // if (Array.isArray($moreDescription) && typeof $moreDescription[0] === 'object') {
-                //     for (newObj of $moreDescription) {
-                //         let newUrl = dndReference.apiUrl + obj.url;
-                //         console.log(newObj);
-                //         if (!obj.url) {
-                //             for (key in newObj) {
-                //                 const $extraTopic = $(`<p class="extra-topic">${key}: ${newObj[key]}</p>`)
-                //                 $('#results').append($moreTopic).append($extraTopic);
-                //             }
-                //         }
-                //         else {
-                //             let $descriptionButton = $(`<button type="button" class="btn btn-secondary">${newObj.name}</button>`);
-                //             $descriptionButton.click(() => {
-                //                 console.log(newUrl);
-                //                 dndReference.buttonHandler(newObj, newUrl);
-                //             })
-
-                //             $('#results').append($moreTopic).append($descriptionButton);
-                //         }
-                //     }
-                // }
-                // else if (!Array.isArray($moreDescription) && typeof $moreDescription === 'object') {
-                //     for (key in $moreDescription) {
-                //         if (!Array.isArray(key) && typeof key === 'object') {
-                //             for (key in $moreDescription) {
-                //                 let $descriptionButton = $(`<button type="button" class="btn btn-secondary">${key.name}</button>`);
-                //                 $descriptionButton.click(() => {
-                //                     dndReference.buttonHandler(obj, key.url);
-                //                 })
-
-                //                 $('#results').append($moreTopic).append($descriptionButton);
-                //             }
-                //         }
-                //     }
-                // }
-                // else {
-                //     $('#results').append($moreTopic).append($moreDescription);
-                // }
+            // Conditional to check for spell damage data.
+            if (!data.damage) {
+                valTopicArr = [descArr, data.range, data.components, data.material, data.attack_type, data.school.name]
+                titleTopicArr = ['Description', 'Range', 'Components', 'Material', 'Attack Type', 'School'];
             }
-            $('#results').append($resultCard).append($moreTitle);
+            else {
+                valTopicArr = [descArr, data.range, data.components, data.material, data.attack_type, data.damage.damage_type.name, data.school.name];
+                titleTopicArr = ['Description', 'Range', 'Components', 'Material', 'Attack Type', 'Damage Type', 'School'];
+            }
+
+            // For every topic, check to see if it exists, if it does, append it to the DOM, otherwise skip it.
+            for (let i = 0; i < valTopicArr.length; i++) {
+                const $resultCard = $(`<div class="card"></div>`)
+                const $cardBody = $('<div class="card-body"></div>')
+                const $moreTopic = $(`<h4 class="card">${titleTopicArr[i]}</h4>`)
+                $('#results').append($resultCard);
+                $resultCard.append($cardBody);
+                if (i === 0) {
+                    for (let desc of valTopicArr[0]) {
+                        const $moreDesc = $(`<p class="card-text">${desc}</p>`)
+                        $cardBody.append($moreTopic);
+                        $cardBody.append($moreDesc);
+                    }
+                } else {
+                    const $moreDesc = $(`<p class="card-text">${valTopicArr[i]}</p>`)
+                    if (!valTopicArr[i]) {
+                        console.log(`Could not find ${titleTopicArr[i]}`)
+                        continue;
+                    }
+                    $cardBody.append($moreTopic);
+                    $cardBody.append($moreDesc);
+                }
+            }
         })
     },
 }
